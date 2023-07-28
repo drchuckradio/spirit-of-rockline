34c9fdbe3c088dbff5dd89571def8b6bac4680e8
drchuckradio <5093235+drchuckradio@users.noreply.github.com>
2023-07-28 18:16:23 -0400

    Spelling

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 pandoc --toc --split-level=2 -o build/spirit-of-rockline.epub meta-epub.txt LICENSE.md 01-a-little-history.md 02-guest-data-and-contribs.md 03-rl-guests.md 04-mrl-guests.md 05-appendix.md
-pandoc -t chunkedhtml --toc --split-level=2 --number-sections -o build/sol-website.zip meta-html.txt LICENSE.md 01-a-little-history.md 02-guest-data-and-contribs.md 03-rl-guests.md 04-mrl-guests.md 05-appendix.md
+pandoc -t chunkedhtml --toc --split-level=2 --number-sections -o build/srl-website.zip meta-html.txt LICENSE.md 01-a-little-history.md 02-guest-data-and-contribs.md 03-rl-guests.md 04-mrl-guests.md 05-appendix.md
